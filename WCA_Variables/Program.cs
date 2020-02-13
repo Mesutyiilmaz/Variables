1a4731a167e5fd073e41b0866a7188c963cfaf69
Mesutyiilmaz <smesutyilmaz@gmail.com>
2020-02-13 12:43:15 +0300

    Add project files.

---
+++ WCA_Variables/Program.cs
@@ -0,0 +1,46 @@
+﻿ using System;
+
+namespace WCA_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            {
+                byte sayi1 = 56;
+                sbyte sayi2 = 12;
+                short sayi3 = 134;
+                ushort sayi4 = 123;
+                int sayi5 = 324;
+                uint sayi6 = 21432432;
+                long sayi7 = 324234;
+                ulong sayi8 = 23424;
+                float sayi9 = 12.0F;
+                double sayi10 = 12.0;
+                decimal sayi11 = 12.0M;
+                char karakter = 'A';
+                string metin = "Bilge Adam Beşiktaş Yazılım Dersler";
+                string tekKarakter = "A";
+
+                Console.WriteLine(metin);
+                string tabBosluk = "Bilge\tAdam";
+                Console.WriteLine(tabBosluk);
+
+                String metin2 = "Bilge Adam Beşiktaş \"Yazılım\" Dersleri ";
+                Console.WriteLine(metin2);
+                bool result = 10 > 2;
+                Console.WriteLine(result);
+
+
+                Console.ReadKey();
+
+
+
+
+
+
+            }
+        }
+    }
+}
